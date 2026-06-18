27f830a90c94a82d6b151778a35ff5018edb6aa7
hum2006 <alubokho24.nandwa@student.cuk.ac.ke>
2026-06-18 12:43:03 +0300

    final

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 
+function calculateTax(amount) {
+  return amount * 0.10;
+}
+
+function convertToUpperCase(str) {
+  return str.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+  if (num1 >= num2) {
+  return num1;
+} else {
+  return num2;
+}
+;
+}
+
+function isPalindrome(str) {
+  const normalized = str.toLowerCase();
+  const reversed = normalized.split('').reverse().join('');
+  return normalized === reversed;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice - (originalPrice * discountPercentage / 100);
+}
+
+console.log(calculateTax(100));
+console.log(convertToUpperCase("messi"));
+console.log(findMaximum(60,61));
+console.log(isPalindrome("Hello"));
+console.log(isPalindrome("Radar"));
+console.log(calculateDiscountedPrice(10000, 20));
+
+
+ // This is required for the test to function properly  
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice
+};
 
 
 
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
